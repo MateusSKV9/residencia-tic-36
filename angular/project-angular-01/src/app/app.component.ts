6c50ca5a5684d0fccaeb897e4db775bde5f2a8ba
Mateus Santos <mateussilvanzg@gmail.com>
2024-10-19 14:58:30 -0300

    routes added

--- angular/project-angular-01/src/app/app.component.ts
+++ angular/project-angular-01/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { AboutComponent } from "./pages/about/about.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
